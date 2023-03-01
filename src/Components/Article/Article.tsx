19224b3b3b0b065784a5dd6efeeb104d7859e944
Adelle Pitsas <apitsas4@gmail.com>
2023-03-01 13:52:35 -0500

    add sections and display articles

--- src/Components/Article/Article.tsx
+++ src/Components/Article/Article.tsx
@@ -1,1 +1,15 @@
 import React from "react";
+import { cleanThumbnail } from "../../Interfaces/Interfaces";
+
+const Article = ({ thumbnail } : {thumbnail: cleanThumbnail}) => {
+  console.log(thumbnail)
+  return (
+    <div>
+      <h3>{thumbnail.title}</h3>
+      <h4>{thumbnail.subsection}</h4>
+      <p>{thumbnail.publishedDate}</p>
+    </div>
+  )
+}
+
+export default Article
--- src/Components/ArticleContainer/ArticleContainer.tsx
+++ src/Components/ArticleContainer/ArticleContainer.tsx
@@ -1,11 +1,27 @@
 import React from "react";
 import { cleanThumbnail } from "../../Interfaces/Interfaces";
+import ArticleSection from '../ArticleSection/ArticleSection'
 
 const ArticleContainer = ({ thumbnails } : {thumbnails: cleanThumbnail[]}) => {
-  console.log(thumbnails)
+  const articlesList = thumbnails.reduce((acc:any, article) => {
+    if(!acc[article.section]) {
+      acc[article.section] = []
+    } 
+    acc[article.section].push(article)
+    return acc
+  }, {})
+
+  const sections = Object.keys(articlesList).map(section => {
+    return <ArticleSection
+      section={section} 
+      thumbnails={articlesList[section]}
+    />
+  })
+
+  console.log(articlesList)
   return(
     <div>
-
+      {sections}
     </div>
   )
 }
